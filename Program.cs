5e75e3e9b7900cafe433504bfd6747aeea70086c
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-06-19 12:49:43 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Посчитать колличество элеметнов в заданном отрезке массива
+
+int [] array = new int [12];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-99, 100);
+}
+void PrintArray(int [] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        Console.Write(collection[i] + " ");
+    }
+}
+
+void Check()
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    
+     if (array[i] > 9 && array[i] < 100)
+     {
+         count++;
+     }
+    }
+Console.WriteLine(count);
+}
+
+PrintArray(array);
+Console.WriteLine();
+Check();
